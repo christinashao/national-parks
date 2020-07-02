e301b17b9bd012d4bdaaf31ed8d2bf13268f14aa
Christina Shao <christina.shao@berkeley.edu>
2020-07-01 18:40:36 -0700

    v0.2

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-var coll = document.getElementsByClassName("collapsible");
-for (let i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        var content = this.nextElementSibling;
-        if (content.style.maxHeight) {
-            content.style.maxHeight = null;
-        } else {
-            content.style.maxHeight = content.scrollHeight + "px";
-        }
-    });
-}
+(() => {
+    $('#popup-acorn').hide();
+    $('#popup-acorn-text').hover(
+        () => { $('#popup-acorn').show(); },
+        () => { $('#popup-acorn').hide(); }    
+    );
+})();
+
+// var coll = document.getElementsByClassName("collapsible");
+// for (let i = 0; i < coll.length; i++) {
+//     coll[i].addEventListener("click", function () {
+//         this.classList.toggle("active");
+//         var content = this.nextElementSibling;
+//         if (content.style.maxHeight) {
+//             content.style.maxHeight = null;
+//         } else {
+//             content.style.maxHeight = content.scrollHeight + "px";
+//         }
+//     });
+// }
